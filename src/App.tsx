6c3f49791cfda8a206f793f875ac04755bade9cf
charankamal20 <charanksingh20@gmail.com>
2024-02-15 00:07:12 +0530

    Create vite app with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
+import LandingPage from "./components/LandingPage";
 
 function App() {
 
   return (
     <>
-      <div className="text-9xl text-red-600 font-bold">
-        <header className="">
-          <h1>Portfolio</h1>
-        </header>
-      </div>
+      <LandingPage />
     </>
   )
 }
