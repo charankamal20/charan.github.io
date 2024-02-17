d5615e8f085c09a086352b46fefaa0bfc94f5b9a
charankamal20 <charanksingh20@gmail.com>
2024-02-18 03:30:30 +0530

    Add Hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,22 @@
 import Blog from "./pages/Blog";
 import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Projects from "./pages/Projects";
 import Resources from "./pages/resources";
 
-const router = createBrowserRouter([
-  {
-    path: "/portfolio-react/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/blog",
-    element: <Blog />,
-  },
-  {
-    path: "*",
-    element: <ErrorPage />,
-  },
-  {
-    path: "/projects",
-    element: <Projects />,
-  },
-  {
-    path: "/resources",
-    element: <Resources />,
-  },
-  {
-    path: "/more",
-    element: <ErrorPage />,
-  },
-]);
-
 function App() {
-
-  return <RouterProvider router={router} />;
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/resources" element={<Resources />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+    </>
+  );
 }
 
 export default App;
